40b70506432f0718782384063e3d453523afb999
guimaraesgui <guimaraes11santos1@gmail.com>
2022-12-09 18:35:35 -0300

    Update autores_rotas.js
    criação das outras rotas de autores

--- rotas/autores_rotas.js
+++ rotas/autores_rotas.js
@@ -3,6 +3,10 @@ const rota = express.Router();
 
 const autoresController = require('../controller/autores_controller');
 
+rota.get('/', autoresController.listar);
 rota.post('/', autoresController.inserir);
+rota.get('/:id', autoresController.buscarPorId);
+rota.put('/:id', autoresController.atualizar);
+rota.delete('/:id', autoresController.deletar);
 
 module.exports = rota
