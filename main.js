435b7636148893929628012f18925a900b71cf64
guimaraesgui <guimaraes11santos1@gmail.com>
2022-12-10 01:38:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const porta = 3000;
 const autorRota = require('./rotas/autores_rotas');
+const livroRota = require('./rotas/livros_rotas')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api/autores', autorRota);
+app.use('/api/livros', livroRota);
 
 app.listen(porta, () => {
     console.log(`API na porta ${porta}`);
