91473c4e95d984e5b89237a97e8fe68fd8abb986
guimaraesgui <guimaraes11santos1@gmail.com>
2022-12-12 11:15:46 -0300

    Update livros_controller.js

--- controller/livros_controller.js
+++ controller/livros_controller.js
@@ -39,8 +39,71 @@ async function buscarPorAutor(req, res){
     }
 }
 
+
+async function buscarPorNome(req, res){
+    const titulo = req.params.nome;
+
+    try {
+        const livroTitulo = await livroNegocio.buscarPorNome(titulo);
+        res.json(livroTitulo);
+    } catch (err) {
+        if (err && err.id) {
+            res.status(err.id).json({Erro: err.mensagem})
+        } else {
+            res.status(500).json({Erro: "Erro na API"});
+        }
+    }
+}
+
+
+async function buscarPorDisponibilidade(req, res){
+    const disp = req.params.disponibilidade;
+
+    try {
+        const livroDisp = await livroNegocio.buscarPorDisponibilidade(disp);
+        res.json(livroDisp);
+    } catch (err) {
+        if (err && err.id) {
+            res.status(err.id).json({Erro: err.mensagem})
+        } else {
+            res.status(500).json({Erro: "Erro na API"});
+        }
+    }
+}
+
+
+async function buscarPorISBN(req, res){
+    const isbn = req.params.isbn;
+
+    try {
+        const livroISBN = await livroNegocio.buscarPorISBN(isbn);
+        res.json(livroISBN);
+    } catch (err) {
+        if (err && err.id) {
+            res.status(err.id).json({Erro: err.mensagem})
+        } else {
+            res.status(500).json({Erro: "Erro na API"});
+        }
+    }
+}
+
+async function atualizar(req, res){
+    const isbn = req.params.isbn;
+    const livro = req.body;
+
+    try {
+        const livroAtualizado = await livroNegocio.atualizar(livro, isbn);
+        res.status(201).res.json(livroAtualizado)
+    } catch (err) {
+        
+    }
+}
+
 module.exports = {
     listar,
     inserir,
-    buscarPorAutor
+    buscarPorAutor,
+    buscarPorNome,
+    buscarPorDisponibilidade,
+    buscarPorISBN,
 }
