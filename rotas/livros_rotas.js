48382edaaa1658041471d94227ecea0b3f3e3e9d
guimaraesgui <guimaraes11santos1@gmail.com>
2022-12-12 11:15:55 -0300

    Update livros_rotas.js

--- rotas/livros_rotas.js
+++ rotas/livros_rotas.js
@@ -5,6 +5,9 @@ const livrosController = require('../controller/livros_controller');
 
 rota.get('/', livrosController.listar);
 rota.post('/', livrosController.inserir);
-rota.get('/autor/:autor', livrosController.buscarPorAutor)
+rota.get('/autor/:autor', livrosController.buscarPorAutor);
+rota.get('/titulo/:nome', livrosController.buscarPorNome);
+rota.get('/disponibilidade/:disponibilidade', livrosController.buscarPorDisponibilidade);
+rota.get('/isbn/:isbn', livrosController.buscarPorISBN);
 
 module.exports = rota
